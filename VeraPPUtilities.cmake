6fdd2679060c74e6ce78ae0cf9d0d9086e18942f
Sam Spilsbury <smspillaz@gmail.com>
2013-04-13 01:15:25 +0800

    Add "src" in the current source dir

--- VeraPPUtilities.cmake
+++ VeraPPUtilities.cmake
@@ -22,7 +22,7 @@
 set (VERAPP_EXIT_FAILURE_ON_STDERR_WRAPPER_EXECUTABLE
      veracpp_exit_failure_on_stderr_wrapper)
 
-add_subdirectory (src)
+add_subdirectory (${CMAKE_CURRENT_SOURCE_DIR}/src)
 
 function (verapp_list_files_in_external_directory DIRECTORY MATCH RETURN_FILES)
     find_program (_verapp_ls ls)
