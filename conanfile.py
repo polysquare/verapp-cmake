f762eaa917ec03e40b7d1ffd0ec9174da90f049b
Sam Spilsbury <smspillaz@gmail.com>
2016-03-07 17:34:00 +0800

    conan: verapp-target-cmake -> verapp-cmake

--- conanfile.py
+++ conanfile.py
@@ -2,24 +2,24 @@ from conans import ConanFile
 from conans.tools import download, unzip
 import os
 
-VERSION = "0.0.3"
+VERSION = "0.0.2"
 
 
 class VeraPPTargetCmakeConan(ConanFile):
-    name = "verapp-target-cmake"
+    name = "verapp-cmake"
     version = os.environ.get("CONAN_VERSION_OVERRIDE", VERSION)
     generators = "cmake"
     requires = ("cmake-include-guard/master@smspillaz/cmake-include-guard",
                 "tooling-find-pkg-util/master@smspillaz/tooling-find-pkg-util",
                 "tooling-cmake-util/master@smspillaz/tooling-cmake-util",
                 "cmake-unit/master@smspillaz/cmake-unit")
-    url = "http://github.com/polysquare/verapp-target-cmake"
+    url = "http://github.com/polysquare/verapp-cmake"
     license = "MIT"
 
     def source(self):
-        zip_name = "verapp-target-cmake.zip"
+        zip_name = "verapp-cmake.zip"
         download("https://github.com/polysquare/"
-                 "verapp-target-cmake/archive/{version}.zip"
+                 "verapp-cmake/archive/{version}.zip"
                  "".format(version="v" + VERSION),
                  zip_name)
         unzip(zip_name)
@@ -28,9 +28,9 @@ class VeraPPTargetCmakeConan(ConanFile):
     def package(self):
         self.copy(pattern="Find*.cmake",
                   dst="",
-                  src="verapp-target-cmake-" + VERSION,
+                  src="verapp-cmake-" + VERSION,
                   keep_path=True)
         self.copy(pattern="*.cmake",
-                  dst="cmake/verapp-target-cmake",
-                  src="verapp-target-cmake-" + VERSION,
+                  dst="cmake/verapp-cmake",
+                  src="verapp-cmake-" + VERSION,
                   keep_path=True)
